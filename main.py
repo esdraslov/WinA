238bbd7f0757af5a6d563aa75407dc92b038f44c
esdraslov <edvan.marcelino56@gmail.com>
2023-04-28 14:27:17 -0300

    a quit command

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import keyboard
+
 def main():
+    print('WinA 1.0.0, x64, null:')
     while True:
         line = input(">")
+        if line == 'quit()' or keyboard.is_pressed('ctrl+c'): break
 
 if __name__ == '__main__':
     main()
