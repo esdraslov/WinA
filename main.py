a10c40ee0587540f5e34f75166efbb2b9a78b409
esdraslov <edvan.marcelino56@gmail.com>
2023-04-28 14:29:00 -0300

    remove keybind 'ctrl+c'

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import keyboard
-
 def main():
     print('WinA 1.0.0, x64, null:')
     while True:
         line = input(">")
-        if line == 'quit()' or keyboard.is_pressed('ctrl+c'): break
+        if line == 'quit()': break
 
 if __name__ == '__main__':
     main()
